854371f664ca39a2cd851facc8120fa037e4ced8
skconan <supakit.kr@gmail.com>
2017-11-26 16:48:29 +0700

    add lib and created oil gland features

--- report/image_processing/lib.py
+++ report/image_processing/lib.py
@@ -5,6 +5,7 @@ import numpy as np
 def get_mode(channel):
     # numpy return a contiguous flattened array.
     data = channel.ravel()
+    data = np.array([x for x in data if x != 0])
     # data = np.array(data)
     if len(data.shape) > 1:
         data = data.ravel()
@@ -29,3 +30,118 @@ def get_kernel(shape='rect', ksize=(5, 5)):
         return np.uint8(kernel)
     else:
         return None
+
+def cut_contours(M, w, h, range_w, range_h):
+    cx = None
+    cy = None
+    try:
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+    except:
+        print('err')
+    if cx is None:
+        return True
+    if cx <= range_w or cy <= range_h or cx >= w - range_w or cy >= h - range_h:
+        return True
+    return False
+
+
+def brightness(imgBGR, brightnessValue):
+    hsv = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    v = np.int16(v)
+    v = np.clip(v + brightnessValue, 0, 255)
+    v = np.uint8(v)
+    hsv = cv2.merge((h, s, v))
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+
+def clip_v(imgBGR, min, max):
+    hsv = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    v = np.clip(v, min, max)
+    hsv = cv2.merge((h, s, v))
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+
+def equalization_bgr(imgBGR):
+    b, g, r = cv2.split(imgBGR)
+    b = cv2.equalizeHist(b)
+    g = cv2.equalizeHist(g)
+    r = cv2.equalizeHist(r)
+    equBGR = cv2.merge((b, g, r))
+    return equBGR
+
+
+def equalization_hsv(imgHSV):
+    h, s, v = cv2.split(imgHSV)
+    s = cv2.equalizeHist(s)
+    v = cv2.equalizeHist(v)
+    equHSV = cv2.merge((h, s, v))
+    return equHSV
+
+
+def equalization_gray(imgGRAY):
+    equGRAY = cv2.equalizeHist(imgGRAY)
+
+    return equGRAY
+
+
+def clahe(imgBGR):
+    lab = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2Lab)
+    l, a, b = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    l = clahe.apply(l)
+    lab = cv2.merge((l, a, b))
+    resBGR = cv2.cvtColor(lab, cv2.COLOR_Lab2BGR)
+    return resBGR
+
+
+def stretching_hsv(hsv):
+    h, s, v = cv2.split(hsv)
+
+    if s.min() > 0:
+        s *= int(round(255.0 / (s.max() - s.min())))
+    if v.min() > 0:
+        v *= int(round(255.0 / (v.max() - v.min())))
+    hsv = cv2.merge((h, s, v))
+    return hsv
+
+
+def stretching_bgr(bgr):
+    b, g, r = cv2.split(bgr)
+    b -= b.min()
+    b *= int(round(255.0 / (b.max() - b.min())))
+    g -= g.min()
+    g *= int(round(255.0 / (g.max() - g.min())))
+    r -= r.min()
+    r *= int(round(255.0 / (r.max() - r.min())))
+
+    img = cv2.merge((b, g, r))
+    return img
+
+
+def stretching(img):
+
+    b, g, r = cv2.split(img)
+    b -= b.min()
+    b *= int(round(255.0 / (b.max() - b.min())))
+    g -= g.min()
+    g *= int(round(255.0 / (g.max() - g.min())))
+    r -= r.min()
+    r *= int(round(255.0 / (r.max() - r.min())))
+
+    img = cv2.merge((b, g, r))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(img)
+
+    s -= s.min()
+    s *= int(round(255.0 / (s.max() - s.min())))
+
+    v -= v.min()
+    v *= int(round(255.0 / (v.max() - v.min())))
+
+    img = cv2.merge((h, s, v))
+    img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+
+    return img
--- report/image_processing/oil_gland_features.py
+++ report/image_processing/oil_gland_features.py
@@ -1,3 +1,48 @@
 import numpy as np
 from lib import *
+import constant as CONST
 
+debug = False
+
+
+def print_debug(msg):
+    global debug
+    if debug:
+        print(str(msg))
+
+def oil_gland_features(img):
+    # convert bgr to hsv
+    img = equalization_bgr(img)
+    b, g, r = cv2.split(img)
+
+    # get h value that appears most often
+    b_mode = get_mode(b)
+    g_mode = get_mode(g)
+    r_mode = get_mode(r)
+    constt = 20
+    # find circle from inrange
+    lower_bound = np.array([max(b_mode - constt,0), 0, 0], dtype=np.uint8)
+    upper_bound = np.array([min(b_mode + constt,255),255, 255], dtype=np.uint8)
+    print(lower_bound,upper_bound)
+    res_inrange = cv2.inRange(img, lower_bound, upper_bound)
+    cv2.imshow('res',res_inrange)
+    cv2.imshow('img',img)
+    cv2.imshow('img_b',b)
+    cv2.waitKey(0)
+    return None
+def main():
+    # symbol = ['A', 'B', 'C', 'D', 'E', 'F']
+    symbol = ['A']
+    error_list = []
+    for prefix in symbol:
+        for i in range(1, 2):
+            for j in range(1, 9):
+                img_name = prefix + str(i) + '_2017110' + str(j) + '.JPG'
+                img = cv2.imread(CONST.IMG_SAVE_PATH+img_name, 1)
+                res = oil_gland_features(img)
+                print(res)
+
+
+if __name__ == '__main__':
+    main()
+    pass
